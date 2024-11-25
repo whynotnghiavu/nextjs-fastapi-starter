bb1472116219216b555a5ad5b0e4326046660f0f
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-11-26 01:04:47 +0700

    update api/index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 
-### Create FastAPI instance with custom docs and openapi url
-app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+app = FastAPI(
+    docs_url="/docs",
+    openapi_url="/openapi.json"
+)
+
 
 @app.get("/api/py/helloFastApi")
 def hello_fast_api():
     return {"message": "Hello from FastAPI"}
+
+
 @app.get("/api/py/xxxxxxxxxx")
 def hello_fast_api():
     return {"message": "Hello from xxxxxxxxxx"}
