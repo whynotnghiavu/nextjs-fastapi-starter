9eb8e2d2e92f03e98b590d0e736057984feba3af
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-11-26 01:06:56 +0700

    update api/index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
 
 app = FastAPI(
-    docs_url="/docs",
-    openapi_url="/openapi.json"
+    docs_url="/api/py/docs",
+    openapi_url="/api/py/openapi.json"
 )
 
 
 @app.get("/api/py/helloFastApi")
-def hello_fast_api():
+def helloFastApi():
     return {"message": "Hello from FastAPI"}
 
 
 @app.get("/api/py/xxxxxxxxxx")
-def hello_fast_api():
+def xxxxxxxxxx():
     return {"message": "Hello from xxxxxxxxxx"}
+
+
+@app.get("/api/py/cccccccccc")
+def cccccccccc():
+    return {"message": "Hello from cccccccccc"}
